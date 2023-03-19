bf8fbf2aab5d7840c169d98391452b2706f2f198
ayan-de <deayan252@gmail.com>
2023-03-20 02:07:57 +0530

    More routes added on product model

--- tshirtstore/controllers/productController.js
+++ tshirtstore/controllers/productController.js
@@ -1,13 +1,13 @@
-const Product = require("../models/product");
-const BigPromise = require("../middlewares/bigPromise");
-const cloudinary = require("cloudinary");
-const CustomError = require("../utils/customError");
-const WhereClause = require("../utils/whereClause");
+let Product = require("../models/product");
+let BigPromise = require("../middlewares/bigPromise");
+let cloudinary = require("cloudinary");
+let CustomError = require("../utils/customError");
+let WhereClause = require("../utils/whereClause");
 
 exports.addProduct = BigPromise(async (req, res, next) => {
   //images
 
-  let imageArray = [];
+  let imagesArray = [];
 
   if (!req.files) {
     return next(new CustomError(`Images are required`, 401));
@@ -21,17 +21,17 @@ exports.addProduct = BigPromise(async (req, res, next) => {
           folder: "products",
         }
       );
-      imageArray.push({
+      imagesArray.push({
         id: result.public_id,
         secure_url: result.secure_url,
       });
     }
   }
 
-  req.body.photos = imageArray;
+  req.body.photos = imagesArray;
   req.body.user = req.user.id;
 
-  const product = await Product.create(req.body);
+  let product = await Product.create(req.body);
 
   res.status(200).json({
     success: true,
@@ -40,16 +40,15 @@ exports.addProduct = BigPromise(async (req, res, next) => {
 });
 
 exports.getAllProduct = BigPromise(async (req, res, next) => {
-  const resultperPage = 6;
-  const totalcountProduct = await Product.countDocuments();
+  let resultperPage = 6;
+  let totalcountProduct = await Product.countDocuments();
 
-  
-  const productsObj = new WhereClause(Product.find(), req.query)
+  let productsObj = new WhereClause(Product.find(), req.query)
     .search()
     .filter();
 
   let products = productsObj.base;
-  const filterProductNumber = products.length;
+  let filterProductNumber = products.length;
 
   productsObj.pager(resultperPage);
   products = await productsObj.base.clone();
@@ -59,5 +58,96 @@ exports.getAllProduct = BigPromise(async (req, res, next) => {
     products,
     filterProductNumber,
     totalcountProduct,
+  });
+});
+
+exports.getOneProduct = BigPromise(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new CustomError(`No product found with this id`, 401));
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+//admin only controller
+
+exports.adminGetAllProduct = BigPromise(async (req, res, next) => {
+  let products = await Product.find({});
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
+});
+
+exports.adminUpdateOneProduct = BigPromise(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new CustomError(`No product found with this id`, 401));
+  }
+  let imagesArray = [];
+
+  if (req.files) {
+    //destroy the existing images
+    for (let index = 0; index < product.photos.length; index++) {
+      let res = await cloudinary.v2.uploader.destroy(
+        product.photos[index].id
+      );
+    }
+
+    //upload and save the images
+
+    for (let index = 0; index < req.files.photos.length; index++) {
+      let result = await cloudinary.v2.uploader.upload(
+        req.files.photos[index].tempFilePath,
+        {
+          folder: "products", //folder name -> .env
+        }
+      );
+      imagesArray.push({
+        id: result.public_id,
+        secure_url: result.secure_url,
+      });
+    }
+  }
+
+  req.body.photos = imagesArray;
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+exports.adminDeleteOneProduct = BigPromise(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new CustomError(`No product found with this id`, 401));
+  }
+
+
+  //destroy the existing images
+  for (let index = 0; index < product.photos.length; index++) {
+    let res = await cloudinary.v2.uploader.destroy(
+      product.photos[index].id
+    );
+  }
+
+  await product.remove()
+
+  res.status(200).json({
+    success: true,
+    message: "Product was deleted!",
   });
 });
